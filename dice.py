8896c04cf70566e404c7230b5ea26f3c562e842a
Laura Tanase <laura.a.tanase@gmail.com>
2014-04-03 20:40:59 -0400

    6th commit overall (5th on master), fixed side issue + added shebang

--- dice.py
+++ dice.py
@@ -1,6 +1,13 @@
+#! /usr/bin/env python
+
 from specialInput import *
 
 numSides = int_input("Hi. How many sides would you like your dice to have? ")
+
+
+while numSides <= 2:
+    print "Sorry, but that's impossible. Try again."
+    numSides = int_input("Hi. How many sides would you like your dice to have? ")
 
 numDice = int_input("OK. How many dice would you like to roll? ")
 
@@ -8,4 +15,5 @@ import random
 
 for x in range(0, numDice):
     print random.randint(1, numSides)
+
 
