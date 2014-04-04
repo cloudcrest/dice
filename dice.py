0e39ce1cb1128aac1c19690928dc81fbfe341ee2
Laura Tanase <laura.a.tanase@gmail.com>
2014-04-03 20:28:16 -0400

    Initial commit on branch customer (5th commit overall)

--- dice.py
+++ dice.py
@@ -1,3 +1,14 @@
+
+# ask the user how many dice, then ask how many sides each die has. (ie - 5 sided die and 12 sided die)
+
+# keep numSides
+# keep numDice
+# put numsides in forloop.
+
+
+# ------------------------------------------------------------------------------
+
+
 from specialInput import *
 
 numSides = int_input("Hi. How many sides would you like your dice to have? ")
